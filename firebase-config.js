a169c969af9d3deb01e0446c72229649d247a80a
drstuarth <dr.stuarthgonz@gmail.com>
2024-06-04 21:19:27 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBjXLpAO2FIk6aJR-bhNAyXMLYSD20xUBY",
-  authDomain: "srcm-2e013.firebaseapp.com",
-  projectId: "srcm-2e013",
-  storageBucket: "srcm-2e013.appspot.com",
-  messagingSenderId: "263204066963",
-  appId: "1:263204066963:web:469fbb319d63f751ef5f66",
+  apiKey: "AIzaSyAJvIbzNr_YWA5HArA1PgDfuTRiuySj1yw",
+  authDomain: "sdrc-654ac.firebaseapp.com",
+  projectId: "sdrc-654ac",
+  storageBucket: "sdrc-654ac.appspot.com",
+  messagingSenderId: "816986947863",
+  appId: "1:816986947863:web:b5dbd70250de382ef7f752",
 };
 
 firebase.initializeApp(firebaseConfig);
