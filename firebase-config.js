205e8794eea9f44fbcc36e2d54a3a591f759fb61
drstuarth <dr.stuarthgonz@gmail.com>
2024-06-11 14:13:04 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,3 +10,16 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+// Función para cerrar sesión
+function cerrarSesion() {
+    firebase.auth().signOut().then(() => {
+        // Cierre de sesión exitoso
+        console.log("Sesión cerrada correctamente");
+        // Redirigir al usuario a la página de inicio de sesión o a otra página deseada
+        window.location.href = "index.html";
+    }).catch((error) => {
+        // Manejar errores
+        console.error("Error al cerrar sesión:", error);
+    });
+}
