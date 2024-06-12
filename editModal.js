fec0dc80b4c50cc01d66a9ce4f54ac62612dbaf0
drstuarth <dr.stuarthgonz@gmail.com>
2024-06-11 20:45:40 -0600

    Create editModal.js

---
+++ editModal.js
@@ -0,0 +1,10 @@
+// editModal.js
+
+function openEditModal(reservationId, reservationData) {
+    // Lógica para abrir el modal de edición con los datos de la cita seleccionada
+    console.log(`Modal de edición abierto para la cita con ID: ${reservationId}`);
+    console.log('Datos de la cita:', reservationData);
+}
+
+// Exporta la función para que esté disponible en otros archivos
+export { openEditModal };
